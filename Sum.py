b79d9c0c616afcd84b5556aea40e44c14d6f8569
ricaportela <ricaportela@gmail.com>
2022-07-09 20:48:27 -0300

    add sum

---
+++ Sum.py
@@ -0,0 +1,14 @@
+def calc(number):
+    result = []
+    for i in range(1, number):
+        if i % 3 == 0 or i % 5 == 0:
+            result.append(i)
+           
+           
+    print(f'Os multiplo entre {number} sao {result}')
+
+    return sum(result)
+
+number = int(input('Digite o numero: '))
+res = calc(number)
+print(f'A soma é = {res}')
