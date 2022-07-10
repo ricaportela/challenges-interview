282726dae5a961167ec61412ef5116bdf3f644cc
ricaportela <ricaportela@gmail.com>
2022-07-10 06:45:23 -0300

    add votes

---
+++ CalcVotes.py
@@ -0,0 +1,21 @@
+"""
+Considerando a tabela abaixo.
+
+total de eleitores =1000
+valido = 800
+votos brancos 150
+nulos = 50
+
+Faça uma classe com 3 métodos que calculam....
+⚫ percentual do votos válidos em relação ao total de eleitores,
+⚫ percentual de branco em relação ao total de eleitores
+⚫ o percentual de nulos em relação ao total de eleitores
+
+Dica: relação ao total significa que você deve dividir, por exemplo 
+"nulos pelo total de etores, validos pelo total de eleitores, etc...
+
+Utilize programação orientada a objetos"""
+
+class CalcVotes():
+    def __init__(self) -> None:
+        pass
